a3d04764a4982947adc891e263dc1fa908869f6d
Pouya Arvandian <pouya314@gmail.com>
2014-08-06 15:45:18 +0800

    fixed the redis conn config for dev and prod.

--- app/middleware/chat_backend.rb
+++ app/middleware/chat_backend.rb
@@ -12,10 +12,23 @@ class ChatBackend
   def initialize(app)
     @app     = app
     @clients = []
-    uri = URI.parse(ENV["REDISCLOUD_URL"])
-    @redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+    
+    if Rails.env == "production"
+      uri = URI.parse(ENV["REDISCLOUD_URL"])
+      @redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+    else
+      @redis = Redis.new
+    end
+    
     Thread.new do
-      redis_sub = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+      # if Rails.env == "production"
+      #   redis_sub = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+      # else
+      #   redis_sub = Redis.new
+      # end
+      
+      redis_sub = Rails.env == "production" ? Redis.new(host: uri.host, port: uri.port, password: uri.password) : Redis.new
+      
       redis_sub.subscribe(CHANNEL) do |on|
         on.message do |channel, msg|
           @clients.each {|ws| ws.send(msg) }
